85d669b8afa199abc3a1660e8a460cd23117b6a5
David Iglesias Teixeira <ditman@gmail.com>
2023-11-21 17:39:40 -0800

    Make app resize as needed!

--- dynamic_view_sizing/lib/main.dart
+++ dynamic_view_sizing/lib/main.dart
@@ -12,30 +12,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  double _size = 400;
-
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.ltr,
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            _size += 100;
-          });
-        },
-        onSecondaryTap: () {
-          setState(() {
-            _size -= 100;
-          });
-        },
-        child: Container(
-          height: _size,
-          width: _size,
-          color: Colors.yellow,
-          child: Text(
-            "I am $_size x $_size -- Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            style: const TextStyle(color: Colors.black),
+      child: Container(
+        color: Colors.yellow,
+        child: const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(bottom: 16),
+                  child: Text('I am only as big as I need to be!',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ))),
+              Padding(
+                padding: EdgeInsets.only(bottom: 8),
+                child: Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in aliquam lectus. Cras nulla ligula, commodo quis mollis id, faucibus in felis. In lacinia tortor at malesuada suscipit. Sed lobortis bibendum turpis sed efficitur. Ut pulvinar orci augue. Etiam pellentesque, tortor at tristique elementum, odio odio vestibulum orci, at venenatis lacus purus eget massa. Aliquam vulputate ipsum nec arcu blandit, nec tristique nunc mollis. Nullam tempus est nec ex venenatis auctor. Cras vel sapien a massa egestas tempor dictum posuere ante.",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              Text(
+                "Mauris sit amet leo ultricies, eleifend tellus nec, dictum urna. Pellentesque semper malesuada nisi ut ultricies. Curabitur scelerisque molestie feugiat. Sed sagittis venenatis diam eget fermentum. Pellentesque sit amet orci vitae elit fermentum blandit id non justo. Integer ac vulputate ipsum. Cras hendrerit nunc ac sem accumsan porta. Sed blandit consequat dictum. Suspendisse vel faucibus nibh. Phasellus molestie, lorem ut rutrum rutrum, erat justo sollicitudin nulla, nec luctus magna metus sed lorem. Integer dapibus gravida enim nec dapibus. In gravida magna nec mi cursus, in feugiat libero aliquam. Quisque aliquam metus quis metus ullamcorper, sed tempus nisl rhoncus. Nulla facilisi. Donec placerat metus vitae est laoreet finibus.",
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
           ),
         ),
       ),
